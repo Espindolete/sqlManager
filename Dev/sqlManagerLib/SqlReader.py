e742e770982b006e39e34b4bc96c11a169c46f5c
Espindolete <agustinespin@hotmail.com>
2022-12-06 12:50:00 +0000

    flake8 fixes

--- Dev/sqlManagerLib/SqlReader.py
+++ Dev/sqlManagerLib/SqlReader.py
@@ -6,28 +6,24 @@ driver = "{ODBC Driver 17 for SQL Server}"
 
 class sqlManager:
     def __init__(self, server, database, username, password):
-        self.cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='\
-                                    +server+';DATABASE='+database+';ENCRYPT=no;UID='\
-                                    +username+';PWD='+ password+";Trust Server Certificate=yes")
-        self.cursor=self.cnxn.cursor()
+        self.cnxn = pyodbc.connect('DRIVER=' + driver + ';SERVER=' + server + ';DATABASE=' + database + ';ENCRYPT=no;UID=' + username + ';PWD=' + password + ";Trust Server Certificate=yes")
+        self.cursor = self.cnxn.cursor()
 
     def Get5Orders(self):
         return self.cursor.execute("select * from dbo.orders").fetchmany(5)
-    
-    def insertOffice(self,myDict:dict):
-        placeholders = ', '.join(['%s'] * len(myDict))
+
+    def insertOffice(self, myDict: dict):
         columns = ', '.join(myDict.keys())
-        for x,y in myDict.items():
+        for x, y in myDict.items():
             try:
                 int(y)
-                myDict[x]=y
+                myDict[x] = y
             except ValueError:
-                myDict[x]="\'"+y+"\'"
+                myDict[x] = "\'"+y+"\'"
 
-        values=','.join(myDict.values())
+        values = ','.join(myDict.values())
         print(values)
-        sql = "INSERT INTO dbo.offices ( %s ) VALUES ( %s )"\
-                % (columns, values)
+        sql = "INSERT INTO dbo.offices ( %s ) VALUES ( %s )" % (columns, values)
 
         print(sql)
         self.cursor.execute(sql)
